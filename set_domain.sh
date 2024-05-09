2f66abe894e83a4d75e16d56e27084b0d683afb3
datagutten <datagutten@datagutten.net>
2024-05-09 16:34:06 +0200

    Replace volumes with host path for data

---
+++ set_domain.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sed -i "s/DOMAIN.*/DOMAIN=$1/g" ./*/.env
+sed -i "s#DATA_PATH.*#DATA_PATH=$2#g" ./*/.env
+
+echo "DOMAIN=$1">loki/.env
+echo "DOMAIN=$1">prometheus/.env
+
+echo "DATA_PATH=$2">>loki/.env
+echo "DATA_PATH=$2">syslogng/.env
